5716e903ca140f070100360d93bc7aa759e86304
leonardosepulveda <leonardosepulveda@gmail.com>
2020-02-14 12:52:02 -0500

    initial commit

---
+++ slurm/submit/merlin_slurm_BC064_sample_04.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -p zhuang,shared
+#SBATCH -t 7-00:00:00
+#SBATCH --mem 4000
+#SBATCH --open-mode=append
+#SBATCH -o /n/home06/lsepulvedaduran/Software/merfish-parameters/slurm/out/BC064/BC064_sample_04.out
+#SBATCH -e /n/home06/lsepulvedaduran/Software/merfish-parameters/slurm/err/BC064/BC064_sample_04.err
+
+date +'Starting at %R.'
+
+source centos7-modules.sh
+module load Anaconda3/5.0.1-fasrc01
+source activate merlin_env
+module load gcc/8.2.0-fasrc01
+module load fftw
+which python
+echo BC064_sample_04
+
+merlin -k parameters_BC064.json \
+       -a merlin_analysis_BC064.json \
+       -o data_organization_BC064_2.csv \
+       -p positions_BC064_sample_04.txt \
+       -c J4E1_codebook_refseq.csv \
+       -m MERFISH3.json \
+       -n 1000 \
+       190812_BC064_MERFISH/sample_04
+
+date +'Finished at %R.'
---
+++ slurm/submit/merlin_slurm_BC071_sample_02_nuclei_segmentation.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -p zhuang,shared
+#SBATCH -t 7-00:00:00
+#SBATCH --mem 4000
+#SBATCH --open-mode=append
+#SBATCH -o /n/home06/lsepulvedaduran/Software/merfish-parameters/slurm/out/BC071_sample_02_nuc_seg.out
+#SBATCH -e /n/home06/lsepulvedaduran/Software/merfish-parameters/slurm/err/BC071_sample_02_nuc_seg.err
+
+date +'Starting at %R.'
+
+source centos7-modules.sh
+module load Anaconda3/5.0.1-fasrc01
+source activate merlin_env
+module load gcc/8.2.0-fasrc01
+module load fftw
+which python
+echo BC071_sample_02
+
+merlin -k snakemake_parameters.json \
+       -a merlin_analysis_BC071_nuclei_segmentation.json \
+       -o data_organization_BC071_3.csv \
+       -p positions_BC071_sample_02.txt \
+       -c C1E1_codebook.csv \
+       -m MERFISH3.json \
+       -n 1000 \
+       191212_BC071_MERFISH/sample_02
+
+date +'Finished at %R.'
