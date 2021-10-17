b09f7591015854478a174116dd6cc5c06ec2ea68
leonardosepulveda <leonardosepulveda@gmail.com>
2021-10-17 18:42:28 -0400

    bc124

---
+++ slurm/submit/merlin_slurm_BC124_sample_01_1.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -p zhuang,shared
+#SBATCH -t 7-00:00:00
+#SBATCH --mem 4000
+#SBATCH --open-mode=append
+#SBATCH -o /n/home06/lsepulvedaduran/Software/merfish-parameters/slurm/out/BC124_sample_01_1.out
+#SBATCH -e /n/home06/lsepulvedaduran/Software/merfish-parameters/slurm/err/BC124_sample_01_1.err
+
+date +'Starting at %R.'
+
+source centos7-modules.sh
+module load Anaconda3/5.0.1-fasrc01
+source activate merlin_env
+module load gcc/8.2.0-fasrc01
+module load fftw
+which python
+echo BC115_sample_04
+
+merlin -k parameters_BC124.json \
+       -a merlin_analysis_BC124.json \
+       -o data_organization_BC124.csv \
+       -p positions_BC124_sample_01.txt \
+       -c C2v6_codebook_mplx.csv \
+       -m MERFISH8.json \
+       -n 1000 \
+       211008_BC124/sample_01_1
+
+date +'Finished at %R.'
