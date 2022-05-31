1ae966b2a0a8725314398859648ddec06c7de3db
leonardosepulveda <leonardosepulveda@gmail.com>
2022-05-31 09:51:31 -0400

    bc168

--- slurm/submit/merlin_slurm_BC168_sample_05.sh
+++ slurm/submit/merlin_slurm_BC168_sample_05.sh
@@ -5,8 +5,8 @@
 #SBATCH -t 7-00:00:00
 #SBATCH --mem 4000
 #SBATCH --open-mode=append
-#SBATCH -o /n/home06/lsepulvedaduran/Software/merfish-parameters/slurm/out/BC168_sample_01.out
-#SBATCH -e /n/home06/lsepulvedaduran/Software/merfish-parameters/slurm/err/BC168_sample_01.err
+#SBATCH -o /n/home06/lsepulvedaduran/Software/merfish-parameters/slurm/out/BC168_sample_05.out
+#SBATCH -e /n/home06/lsepulvedaduran/Software/merfish-parameters/slurm/err/BC168_sample_05.err
 
 date +'Starting at %R.'
 
@@ -16,17 +16,17 @@ source activate merlin_env
 module load gcc/8.2.0-fasrc01
 module load fftw
 which python
-echo BC168_sample_01
+echo BC168_sample_05
 
 merlin -k parameters_BC168.json \
        -a merlin_analysis_BC168.json \
        -o data_organization_BC168.csv \
-       -p positions_BC168_sample_01_1.txt \
+       -p positions_BC168_sample_05_1.txt \
        -c C2v6_codebook_mplx.csv \
        -m MERFISH3.json \
        -n 1000 \
        -e /n/holyscratch01/zhuang_lab/Users/lsepulvedaduran/data \
        -s /n/holyscratch01/zhuang_lab/Users/lsepulvedaduran/analysis \
-       220506_BC168_data/sample_01
+       220506_BC168_data/sample_05
 
 date +'Finished at %R.'
