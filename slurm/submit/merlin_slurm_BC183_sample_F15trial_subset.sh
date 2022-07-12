5b0012cf1b7f7d3714395794995d81c43a41534e
leonardosepulveda <leonardosepulveda@gmail.com>
2022-07-12 08:22:40 -0400

    add sannes's experiments

---
+++ slurm/submit/merlin_slurm_BC183_sample_F15trial_subset.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#!/bin/bash
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -p zhuang,shared
+#SBATCH -t 7-00:00:00
+#SBATCH --mem 4000
+#SBATCH --open-mode=append
+#SBATCH -o /n/home06/lsepulvedaduran/Software/merfish-parameters/slurm/out/BC183_sample_F15_trail_subset.out
+#SBATCH -e /n/home06/lsepulvedaduran/Software/merfish-parameters/slurm/err/BC183_sample_F15_trail_subset.err
+
+date +'Starting at %R.'
+
+source centos7-modules.sh
+module load Anaconda3/5.0.1-fasrc01
+source activate merlin_env
+module load gcc/8.2.0-fasrc01
+module load fftw
+which python
+echo BC183_sample_F15trial_subset
+
+merlin -k parameters_BC168.json \
+       -a merlin_analysis_BC183_F15_subset.json \
+       -o data_organization_BC183_F15trial.csv \
+       -p positions_BC183_sample_F15trial_subset.txt \
+       -c C2v6_codebook_mplx.csv \
+       -m MERFISH3.json \
+       -n 1000 \
+       -e /n/holyscratch01/zhuang_lab/Users/lsepulvedaduran/data \
+       -s /n/holyscratch01/zhuang_lab/Users/lsepulvedaduran/analysis \
+       sample_F15trial_subset
+
+date +'Finished at %R.'
