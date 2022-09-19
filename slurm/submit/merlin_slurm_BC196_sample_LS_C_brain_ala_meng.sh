98ed8928f82a32c423e716a933026d33d6150c58
leonardosepulveda <leonardosepulveda@gmail.com>
2022-09-18 22:17:45 -0400

    ufff, a lot

---
+++ slurm/submit/merlin_slurm_BC196_sample_LS_C_brain_ala_meng.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -p zhuang
+#SBATCH -t 7-00:00:00
+#SBATCH --mem 4000
+#SBATCH --open-mode=append
+#SBATCH -o /n/home06/lsepulvedaduran/Software/merfish-parameters/slurm/out/BC196_sample_LS_C_brain_ala_meng.out
+#SBATCH -e /n/home06/lsepulvedaduran/Software/merfish-parameters/slurm/err/BC196_sample_LS_C_brain_ala_meng.err
+
+date +'Starting at %R.'
+
+source centos7-modules.sh
+module load Anaconda3/5.0.1-fasrc01
+source activate merlin_env
+module load gcc/8.2.0-fasrc01
+module load fftw
+which python
+echo BC196_sample_LS_C_brain_ala_meng
+
+merlin -k parameters_BC196.json \
+       -a merlin_analysis_BC196_brain_ala_meng.json \
+       -o data_organization_BC196_brain.csv \
+       -p positions_BC196_sample_LS_C_brain.txt \
+       -c MO4E4_codebook.csv \
+       -m MERFISH3.json \
+       -n 1000 \
+       -e /n/holyscratch01/zhuang_lab/Users/lsepulvedaduran/data \
+       -s /n/holyscratch01/zhuang_lab/Users/lsepulvedaduran/analysis \
+       220802_BC196_data/sample_LS_C_brain
+
+date +'Finished at %R.'
